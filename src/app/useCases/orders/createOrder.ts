8ee475c8b4e49f5b4e32c6d00745809ef2111bd4
iagofreitas05 <iagofreitas05@gmail.com>
2023-06-29 22:41:18 +0100

    - real time

--- src/app/useCases/orders/createOrder.ts
+++ src/app/useCases/orders/createOrder.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from "express";
 import { Order } from "../../models/Order";
+import {io} from "../../../index";
 
 export async function createOrder(req: Request, res: Response){
   try{
     const {table, products} = req.body;
-    const category = await Order.create({table, products});
-    res.status(201).json(category);
+    const order = await Order.create({table, products});
+    const orderDetails = await order.populate("products.product");
+    io.emit("orders@new", orderDetails);
+    res.status(201).json(order);
   }catch{
     res.status(500).json({
       error: "Internal server error"
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,32 @@
 import express from "express";
 import mongoose from "mongoose";
 import path from "node:path";
-import { router } from "./router";
+import {router} from "./router";
+import http from "node:http";
+import {Server} from "socket.io";
 
+export const app = express();
+export const server = http.createServer(app);
+export const io = new Server(server);
 
-
-mongoose.connect("mongodb://localhost:27017").then(()=> {
-  const  app = express();
+mongoose.connect("mongodb://localhost:27017").then(() => {
   const port = 3001;
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+  });
+
   app.use(express.json());
   app.use(router);
   app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
-  app.listen(port, ()=> {
+  server.listen(port, () => {
     console.log(`🏳️ the server is running in peace on ${port}`);
   });
 
   console.log("conectado ao mongo");
-}).catch(()=> {
+}).catch(() => {
   console.log("erro ao conectar ao mongo");
 });
-
